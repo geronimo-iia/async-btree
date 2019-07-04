755795ae0f7bc26081b14842b069aef33744845d
Jerome Guibert <jguibert@gmail.com>
2019-07-04 09:19:49 +0200

    complete definition test

---
+++ tests/test_basics.py
@@ -0,0 +1,32 @@
+from curio import run
+from async_btree import FAILURE, SUCCESS, ExceptionDecorator, node_metadata
+
+
+def test_truthy():
+    assert SUCCESS
+    assert Exception()
+    assert [1, 2]
+
+
+def test_falsy():
+    assert not []
+    assert not bool([])
+    assert not FAILURE
+    assert not bool(FAILURE)
+    assert not bool(ExceptionDecorator(Exception()))
+
+
+def test_exception_decorator_falsy():
+    assert bool(Exception())
+    assert not bool(ExceptionDecorator(Exception()))
+    assert str(ExceptionDecorator(Exception("test"))) == str(Exception("test"))
+    assert repr(ExceptionDecorator(Exception("test"))) == repr(Exception("test"))
+
+
+def test_node_metadata_do_not_change_behavior():
+    async def a_func():
+        return 'a'
+
+    assert run(a_func) == 'a'
+    # no change on behavior
+    assert run(node_metadata()(a_func)) == 'a'
---
+++ tests/test_map_filter.py
@@ -0,0 +1,36 @@
+from curio import run
+
+from async_btree import afilter, amap
+
+
+async def inc(a):
+    return a + 1
+
+
+async def even(a):
+    return a % 2 == 0
+
+
+def test_amap_on_iterable():
+    async def process():
+        return [i async for i in amap(inc, [1, 2])]
+
+    assert run(process) == [2, 3]
+
+
+def test_afilter_on_iterable():
+    async def process():
+        return [i async for i in afilter(even, [0, 1, 2, 3, 4])]
+
+    assert run(process) == [0, 2, 4]
+
+
+def test_afilter_amap_aiter():
+    async def process1():
+        return [i async for i in afilter(even, amap(inc, [0, 1, 2, 3, 4]))]
+
+    async def process2():
+        return [i async for i in amap(inc, afilter(even, [0, 1, 2, 3, 4]))]
+
+    assert run(process1) == [2, 4]
+    assert run(process2) == [1, 3, 5]
