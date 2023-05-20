c5cb60031fd5302d49b7679fa0d0c508f237f0e5
Jerome Guibert <jguibert@gmail.com>
2023-05-20 10:20:25 +0200

    test: extends on has_curio

--- tests/test_utils_run.py
+++ tests/test_utils_run.py
@@ -1,9 +1,9 @@
 # from asyncio import run as run_asyncio
-from contextvars import ContextVar
+from contextvars import ContextVar, copy_context
 
 from curio import Kernel
 
-from async_btree.utils import run, run_curio, has_curio
+from async_btree.utils import has_curio
 
 counter = ContextVar("counter", default=5)
 
@@ -15,28 +15,25 @@ async def reset_counter():
     return -1
 
 
-def test_run():
-    assert run
-    if has_curio():
-        assert run == run_curio
-
-
 def test_run_curio_with_separate_contextvar():
-
+    counter.set(5)
     with Kernel() as k:
-        assert run_curio(k, reset_counter) == 0
-        assert run_curio(k, reset_counter) == 0
+        assert copy_context().run(k.run, reset_counter) == 0
+        assert copy_context().run(k.run, reset_counter) == 0
         assert counter.get() == 5
 
     assert counter.get() == 5
 
 
-# def test_run_asyncio_with_separate_contextvar():
+def test_run_curio_with_same_contextvar():
+    counter.set(5)
+    with Kernel() as k:
+        assert k.run(reset_counter) == 0
+        assert k.run(reset_counter) == -1
+        assert counter.get() == 0
 
-#     # from asyncio import Runner only with python > 3.11
-#     # with Runner() as k:
-#     assert run_asyncio(reset_counter) == 0
-#     assert run_asyncio(reset_counter) == 0
-#     assert counter.get() == 5
+    assert counter.get() == 0
 
-#     assert counter.get() == 5
+
+def test_has_curio():
+    assert has_curio()
